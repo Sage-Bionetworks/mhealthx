62507b9fd3f2225efd8832f1e03c3383e0640fc7
= <arno@binarybottle.com>
2016-01-11 20:57:20 -0800

    fix setup

--- setup.sh
+++ setup.sh
@@ -99,7 +99,6 @@ export PYTHONPATH=$PYTHONPATH:$INSTALLS/mhealthx
 mkdir $INSTALLS/ffmpeg
 mkdir $INSTALLS/ffmpeg/ffmpeg_sources
 mkdir $INSTALLS/ffmpeg/ffmpeg_build
-sudo apt-get update
 sudo apt-get -y --force-yes install autoconf automake build-essential libass-dev libfreetype6-dev libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texi2html zlib1g-dev
 
 # Install yasm (ffmpeg dependency):
