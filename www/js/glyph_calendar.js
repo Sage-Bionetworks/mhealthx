87d56897f0d7988d4684d684e8c9015cfa5bef6e
Arno Klein <arno@binarybottle.com>
2016-02-01 01:10:41 -0800

    Stuck on area plot (overlay and x-axis labels)...

--- www/js/glyph_calendar.js
+++ www/js/glyph_calendar.js
@@ -46,13 +46,15 @@ function drawGraphsForMonthlyData() {
     max_value = 11;       // maximum value for randomly generated data
     cap_value = 10;       // value corresponding to radius of circle (e.g., control median)
     empty_value = -1;     // negative value corresponding to missing data
-    dates = [27,3,10,17,24,28,4,
-             11,18,25,29,5,12,19,
-             26,30,6,13,20,27,31,
-             7,14,21,28,1,8,15,
-             22,29,2,9,16,23,30];
-    dates2 = [27,28,29,30,31,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
-              16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+    dates = [31,7,14,21,28,
+             1,8,15,22,29,
+             2,9,16,23,1,
+             3,10,17,24,2,
+             4,11,18,25,3,
+             5,12,19,26,4,
+             6,13,20,27,5]
+//    dates2 = [31,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+//              16,17,18,19,20,21,22,23,24,25,26,27,28,29,1,2,3,4];
 
     // Generate random data:
     data = getDataForMonth(number_of_sides * number_of_bars, max_value, empty_value);
@@ -67,6 +69,8 @@ function drawGraphsForMonthlyData() {
 
     // Create a horizon plot for all dates:
     horizon_plot(); //data, cap_value, empty_value);
+
+    barchart_plot();
 
 
     //--------------------------------------------------------------------
@@ -283,10 +287,14 @@ function drawGraphsForMonthlyData() {
             height = 500 - margin.top - margin.bottom,
             contextHeight = 50,
             contextWidth = width * .5;
+
         var svg = d3.select("#chart2").append("svg")
                     .attr("width", width + margin.left + margin.right)
                     .attr("height", (height + margin.top + margin.bottom));
-                    
+
+        var parseDate = d3.time.format("%y-%b-%d").parse,
+            formatPercent = d3.format(".0%");
+
         d3.csv('./data.csv', createChart);
 
         function createChart(data){
@@ -294,7 +302,11 @@ function drawGraphsForMonthlyData() {
           var activities_post = [];
           var charts = [];
           var maxDataPoint = 0;
-          
+
+          //data.forEach(function(d) {
+          //  d.Date = parseDate(d.Date);
+          //});
+
           /* Loop through first row and get each activity
             and push odd columns into premed and even columns
             into postmed activity arrays to use later */
@@ -334,7 +346,7 @@ function drawGraphsForMonthlyData() {
             }
             
             // D3 needs a date object, let's convert it just one time
-            d.Date = new Date(d.Date,0,1);
+            //d.Date = new Date(d.Date,0,1);
           });
     
           for(var i = 0; i < activitiesCount; i++){
@@ -489,6 +501,108 @@ function drawGraphsForMonthlyData() {
             this.chartContainer.select(".x.axis.bottom").call(this.xAxisBottom);
         }
     }
+
+
+/*
+    //------------------------------------------------------------------------
+    // Bar chart function
+    //------------------------------------------------------------------------
+    function barchart_plot() {  //data, cap_value, empty_value) {
+
+      var margin = {top: 20, right: 20, bottom: 30, left: 40},
+          width = 960 - margin.left - margin.right,
+          height = 500 - margin.top - margin.bottom;
+      
+      var x = d3.scale.ordinal()
+          .rangeRoundBands([0, width], .1);
+      
+      var y = d3.scale.linear()
+          .rangeRound([height, 0]);
+      
+      var color = d3.scale.ordinal()
+          .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+      
+      var xAxis = d3.svg.axis()
+          .scale(x)
+          .orient("bottom");
+      
+      var yAxis = d3.svg.axis()
+          .scale(y)
+          .orient("left")
+          .tickFormat(d3.format(".2s"));
+      
+      var svg = d3.select("body").append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      
+      d3.csv("data.csv", function(error, data) {
+        if (error) throw error;
+      
+        color.domain(d3.keys(data[0]).filter(function(key) { return key !== "State"; }));
+      
+        data.forEach(function(d) {
+          var y0 = 0;
+          d.ages = color.domain().map(function(name) { return {name: name, y0: y0, y1: y0 += +d[name]}; });
+          d.total = d.ages[d.ages.length - 1].y1;
+        });
+      
+        data.sort(function(a, b) { return b.total - a.total; });
+      
+        x.domain(data.map(function(d) { return d.State; }));
+        y.domain([0, d3.max(data, function(d) { return d.total; })]);
+      
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+      
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+          .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Population");
+      
+        var state = svg.selectAll(".state")
+            .data(data)
+          .enter().append("g")
+            .attr("class", "g")
+            .attr("transform", function(d) { return "translate(" + x(d.State) + ",0)"; });
+      
+        state.selectAll("rect")
+            .data(function(d) { return d.ages; })
+          .enter().append("rect")
+            .attr("width", x.rangeBand())
+            .attr("y", function(d) { return y(d.y1); })
+            .attr("height", function(d) { return y(d.y0) - y(d.y1); })
+            .style("fill", function(d) { return color(d.name); });
+      
+        var legend = svg.selectAll(".legend")
+            .data(color.domain().slice().reverse())
+          .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+      
+        legend.append("rect")
+            .attr("x", width - 18)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", color);
+      
+        legend.append("text")
+            .attr("x", width - 24)
+            .attr("y", 9)
+            .attr("dy", ".35em")
+            .style("text-anchor", "end")
+            .text(function(d) { return d; });
+    });
+*/
+
 }
 
 
